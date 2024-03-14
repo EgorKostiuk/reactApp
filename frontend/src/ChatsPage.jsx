68f8fa0115ddbc66b4d3795c273dad2b85ce7648
Egor Kostiuk <127864304+EgorKostiuk@users.noreply.github.com>
2024-03-14 16:47:29 +0200

    + implement ChatsPage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-  return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    '0d707114-84ec-4ea9-a708-718ee336bb83', 
+    props.user.username, 
+    props.user.secret
+  );
+  return (
+    <div style={{ height: '100vh' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+    </div>
+  )
 }
 
 export default ChatsPage
