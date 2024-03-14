140ea2937b194cd7281b62a85fa24e6005e3e5c8
Egor Kostiuk <127864304+EgorKostiuk@users.noreply.github.com>
2024-03-14 16:56:23 +0200

    * change ChatsPage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,15 +1,14 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
 
 const ChatsPage = (props) => {
-  const chatProps = useMultiChatLogic(
-    '0d707114-84ec-4ea9-a708-718ee336bb83', 
-    props.user.username, 
-    props.user.secret
-  );
   return (
     <div style={{ height: '100vh' }}>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+      <PrettyChatWindow 
+        projectId='0d707114-84ec-4ea9-a708-718ee336bb83'
+        username={props.user.username}
+        secret={props.user.username}
+        style={{ height: '100%' }}
+      />
     </div>
   )
 }
